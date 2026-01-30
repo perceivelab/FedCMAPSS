1599a62191e833b9a1d979adb41a29837af3051f
Simone Palazzo <simopal6@gmail.com>
2026-01-30 09:59:40 +0100

    Added main_quick to launch.json

--- system/flcore/servers/serveravg_rul.py
+++ system/flcore/servers/serveravg_rul.py
@@ -49,10 +49,6 @@ class FedAvg_RUL(Server_RUL):
             if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
                 break
 
-        print("\nBest accuracy.")
-        # self.print_(max(self.rs_test_acc), max(
-        #     self.rs_train_acc), min(self.rs_train_loss))
-        print(max(self.rs_test_acc))
         print("\nAverage time cost per round.")
         print(sum(self.Budget[1:])/len(self.Budget[1:]))
 
